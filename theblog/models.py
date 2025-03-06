18922e3febdd4f11f0d0834cd4bd196aedd4c554
Mrudula v <mrudulavaji11@gmail.com>
2025-03-06 16:20:44 +0530

    new updates

--- theblog/models.py
+++ theblog/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    tittle=models.CharField(max_length=250)
+    title=models.CharField(max_length=250)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     body=models.TextField()
 
     def __str__(self):
-        return self.tittle + ' ' + str(self.author)
-      
+        return self.title + ' | ' + str(self.author)
